dfce6bb4a2d06cd91b795ba79c20f0264eddb3e3
Panini <alexanderscaini@gmail.com>
2020-11-09 13:49:35 -0500

    finished

--- main.rb
+++ main.rb
@@ -7,6 +7,45 @@ def game_manager
   player1 = Player.new
   player2 = Player.new
   
+  define_method :next_turn do
+    Turn.new
+    puts "---------Turn#{Turn.count}----------"
+
+    turn_number = Turn.count % 2
+
+    next_question = Question.new
+    if turn_number == 0
+      current_player = 2
+    else 
+      current_player = 1
+    end
+    puts "Player #{current_player}: What is #{next_question.x} + #{next_question.y}?"
+    print "> "
+    player_answer = $stdin.gets.chomp.to_i
+    if player_answer == next_question.answer
+      puts "Player #{current_player}: Thats right, #{next_question.x} + #{next_question.y} = #{next_question.answer}"
+    else 
+      puts "Player#{current_player}: Thats incorrect, #{next_question.x} + #{next_question.y} = #{next_question.answer}"
+      if current_player == 1
+        player1.lives -= 1
+      else 
+        player2.lives -= 1
+      end
+    end
+  end 
+  while player1.lives > 0 && player2.lives > 0
+    puts "P1 lives: #{player1.lives}/3 vs P2 lives: #{player2.lives}/3"
+    next_turn
+  end
+
+  if player1.lives == 0
+    puts "Player 2 Wins!"
+  end
+
+  if player2.lives == 0
+    puts "Player 1 Wins!"
+  end
+
 end
   
 def start
